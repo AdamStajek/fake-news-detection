4bd9b13ba1f46df3ec122f251aefac0da84f45ad
AdamStajek <adasta146@gmail.com>
2025-10-28 17:05:10 +0100

    Add mmcovid loader and exploration

---
+++ evaluation/mmcovid/mmcovid_loader.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from typing import Literal
+
+import pandas as pd
+
+from evaluation.fake_news_dataset import FakeNewsDataset
+
+DATASET_PATH = Path(__file__).parent / "data" / "mmcovid"
+
+class MMCovidLoader(FakeNewsDataset):
+    """MMCovid Dataset Loader - Loads the MMCovid Fake News Dataset(https://github.com/bigheiniu/MM-COVID).
+
+    The MMCovid dataset contains claims about Covid19.
+    """
+
+    def __init__(self, split: Literal["train", "validation"] = "train") -> None:
+        """Create an MMCovidLoader instance.
+
+        Args:
+            split (Literal["train", "validation"],
+            optional): A train or validation split for the dataset. Defaults to "train".
+
+        """
+        dataset = pd.read_csv(DATASET_PATH)[["claim", "label"]]
+        self.dataset = self._split_dataset(dataset, split)
+
+    def __len__(self) -> int:
+        """Get the length of the dataset.
+
+        Returns:
+            int: Length of the dataset.
+
+        """
+        return len(self.dataset)
+
+    def __get_item__(self, idx: int) -> tuple[str, int]:
+        """Get an item from the dataset by index.
+
+        Args:
+            idx (int): Index of the item to retrieve.
+
+        Returns:
+            tuple[str, int]: A tuple containing the claim text and its label.
+
+        """
+        return self.dataset.iloc[idx]["claim"], self.dataset.iloc[idx]["label"]
